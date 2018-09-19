0381e6d253645f4e21118e35b65098dcbb1c900d
coord.e <me@coord-e.com>
2018-09-20 04:06:55 +0900

    [WIP] Implementing commands

--- wire_reciever.h
+++ wire_reciever.h
@@ -25,6 +25,20 @@ uint8_t dispatch_address(uint8_t addr, uint8_t value) {
   switch (addr) {
     case 0x09:
       return command::who_am_i();
+    case 0x0A:
+    case 0x0B:
+      return command::get_rx(addr - 0x0A);
+    case 0x0C:
+    case 0x0D:
+      return command::get_ry(addr - 0x0C);
+    case 0x1A:
+    case 0x1B:
+      return command::get_lx(addr - 0x1A);
+    case 0x1C:
+    case 0x1D:
+      return command::get_ly(addr - 0x1C);
+    case 0x20:
+      return command::measure();
     default:
       return 0xff;
   }
@@ -32,8 +46,29 @@ uint8_t dispatch_address(uint8_t addr, uint8_t value) {
 
 namespace command {
 
+static uint16_t rx_cache, ry_cache, lx_cache, ly_cache;
+
+uint16_t float_to_word(float in) {
+  return (in / 2 + 0.5) * 0xFFFF;
+}
+
+uint8_t measure() {
+  float rx, ry, lx, ly;
+  measure::R_estimator->get_center_of_gravity(&rx, &ry);
+  measure::L_estimator->get_center_of_gravity(&lx, &ly);
+  rx_cache = float_to_word(rx);
+  ry_cache = float_to_word(ry);
+  lx_cache = float_to_word(lx);
+  ly_cache = float_to_word(ly);
+  return 0x00;
+}
+
 uint8_t who_am_i() {
   return 0x09;
+}
+
+uint8_t get_rx(uint8_t idx) {
+  return 
 }
 
 };
